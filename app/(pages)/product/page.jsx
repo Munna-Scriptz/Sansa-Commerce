baa58e4203f89452d5aade843e62ad7183acab37
Munna-Scripts <munnascriptz@gmail.com>
2025-09-22 23:00:44 +0600

    Product Page Complete

--- app/(pages)/product/page.jsx
+++ app/(pages)/product/page.jsx
@@ -1,5 +1,9 @@
 import BreadCrumb from '@/app/components/common/BreadCrumb'
 import Tabs from '@/app/components/common/Tabs'
+import BestSelling from '@/app/components/product/BestSelling'
+import NewProducts from '@/app/components/product/NewProducts'
+import RecoProducts from '@/app/components/product/RecoProducts'
+import SpecialOffer from '@/app/components/product/SpecialOffer'
 import React from 'react'
 
 const page = () => {
@@ -8,6 +12,14 @@ const page = () => {
       <BreadCrumb pageName={'Product'}/>
       {/* ------------Includes search text, searchbox, product tabs------------- */}
       <Tabs/>
+      {/* ------------Best Selling Products------------- */}
+      <BestSelling />
+      {/* ------------New Products------------- */}
+      <NewProducts />
+      {/* ------------Recommended Products------------- */}
+      <RecoProducts />
+      {/* ------------Special Offer Products------------- */}
+      <SpecialOffer />
     </>
   )
 }
---
+++ app/components/product/BestSelling.jsx
@@ -0,0 +1,67 @@
+'use client'
+import React from 'react'
+import CommonHead from '../common/CommonHead'
+import Link from 'next/link'
+import { FaPlus } from 'react-icons/fa6'
+import Slider from 'react-slick'
+import img1 from '../../../public/previewImg1.png'
+import img2 from '../../../public/previewImg2.png'
+import img3 from '../../../public/previewImg3.png'
+import img4 from '../../../public/previewImg4.png'
+import SingleSeller from '../common/SingleSeller'
+import "slick-carousel/slick/slick.css";
+
+const BestSelling = () => {
+    const settings = {
+        className: "center",
+        arrows: false,
+        centerMode: true,
+        infinite: true,
+        centerPadding: "60px",
+        slidesToShow: 3,
+        speed: 500,
+        autoplay: true,
+    }
+    
+    const myPro = [
+            {
+                img: img1,
+            },
+            {
+                img: img2,
+            },
+            {
+                img: img3,
+            },
+            {
+                img: img4,
+            },
+    ]
+  return (
+    <>
+        <section id='Best-Selling' className='mt-[48px]'>
+            <div className="container">
+                <div id="Best-Selling-Row">
+                    {/* --------------------Header Text--------------------- */}
+                    <div className='flex items-center justify-between'>
+                        <CommonHead text={'Best Selling Products:'}/>
+                        <Link className='text-primary text-xl flex items-center gap-2 font-bold' href={'/'}>See More <FaPlus /></Link>
+                    </div>
+                    {/* --------------------Product Slider--------------------- */}
+                    <div className="slider-container mt-[77px]">
+                        <Slider {...settings}>
+                            {
+                                myPro.map((item , i)=>(
+                                    <SingleSeller img={item.img} key={i}/>
+                                ))
+                            }
+                        </Slider>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default BestSelling
