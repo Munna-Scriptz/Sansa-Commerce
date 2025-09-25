0bae490c72264e5f2e73c2bd7e7f82e4b3b61731
Munna-Scripts <munnascriptz@gmail.com>
2025-09-25 16:52:18 +0600

    Counter Added

--- app/components/home/Counter.jsx
+++ app/components/home/Counter.jsx
@@ -1,7 +1,8 @@
 'use client'
 import React from 'react'
 import CommonHead from '../common/CommonHead'
-import CountUp from 'react-countup';
+import CountUp from '../common/CountUp'
+
 const Counter = () => {
   return (
     <>
@@ -12,27 +13,28 @@ const Counter = () => {
                     <CommonHead text={'Why You Picking Us'}/>
                     {/* -------------------Counter------------------------- */}
                     <div className='mt-[68px] flex items-center justify-between'>
+                        
                         <div className='p-6 border-1 border-[#C5C8B9] rounded-[12px] w-[280px]'>
-                            <h2 className='text-primary font-bold text-[56px] font-roboto'>
-                                <CountUp start={0} end={1000} useEasing={true} redraw={true}/>+
-                            </h2>
+                            <div className='text-primary font-bold text-[56px] font-roboto'>
+                                <CountUp from={0} to={100} direction="up" duration={2}/>+
+                            </div>
                             <p className='text-subText font-roboto text-lg'>Brands</p>
                         </div>
                         <div className='p-6 border-1 border-[#C5C8B9] rounded-[12px] w-[280px]'>
                             <h2 className='text-primary font-bold text-[56px] font-roboto'>
-                                <CountUp start={0} end={3500} useEasing={true} redraw={true}/>+
+                                <CountUp from={0} to={3500} direction="up" duration={2}/>+
                             </h2>
                             <p className='text-subText font-roboto text-lg'>Customers</p>
                         </div>
                         <div className='p-6 border-1 border-[#C5C8B9] rounded-[12px] w-[280px]'>
                             <h2 className='text-primary font-bold text-[56px] font-roboto'>
-                                <CountUp start={0} end={350} useEasing={true} redraw={true}/>+
+                                <CountUp from={0} to={350} direction="up" duration={2}/>+
                             </h2>
                             <p className='text-subText font-roboto text-lg'>Partners</p>
                         </div>
                         <div className='p-6 border-1 border-[#C5C8B9] rounded-[12px] w-[280px]'>
                             <h2 className='text-primary font-bold text-[56px] font-roboto'>
-                                <CountUp start={0} end={50} useEasing={true} redraw={true}/>+
+                                <CountUp from={0} to={50} direction="up" duration={2}/>+
                             </h2>
                             <p className='text-subText font-roboto text-lg'>Employees</p>
                         </div>
--- app/loading.jsx
+++ app/loading.jsx
@@ -1,4 +1,4 @@
 export default function Loading() {
   // Or a custom loading skeleton component
-  return <div className='LoaderDiv z-10 absolute top-0 left-0 flex items-center justify-center w-full h-screen bg-[#0000007a]'><div class="loader"></div></div>
+  return <div className='LoaderDiv z-10 absolute top-0 left-0 flex items-center justify-center w-full h-screen bg-[#0000007a]'><div className="loader"></div></div>
 }
