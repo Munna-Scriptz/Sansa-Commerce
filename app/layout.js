a4334349a6ac5885334db4db45e6832a27cae606
Munna-Scripts <munnascriptz@gmail.com>
2025-10-07 18:03:29 +0600

    redux added

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import ResNavbar from "./components/common/ResNavbar";
 import "./globals.css";
 import { Roboto } from "next/font/google";
 import { ProgressBar, ProgressBarProvider } from "react-transition-progress";
+import { StoreProvider } from "./redux/StoreProvider";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400"] });
 
@@ -15,16 +16,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${roboto.className} `}>
-        <ProgressBarProvider >
-          <ProgressBar className="fixed z-[999] h-1 shadow-lg shadow-sky-500/20 bg-[#8AC732] top-0" />
-          <Navbar />
-          <ResNavbar />
-          {children}
-          <Footer />
-        </ProgressBarProvider>
-      </body>
-    </html>
+    <StoreProvider>
+      <html lang="en">
+        <body className={`${roboto.className} `}>
+          <ProgressBarProvider >
+            <ProgressBar className="fixed z-[999] h-1 shadow-lg shadow-sky-500/20 bg-[#8AC732] top-0" />
+            <Navbar />
+            <ResNavbar />
+            {children}
+            <Footer />
+          </ProgressBarProvider>
+        </body>
+      </html>
+    </StoreProvider>
   );
 }
