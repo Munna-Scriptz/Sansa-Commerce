33d54e220572d46b4eb827ccf63b4533c5c0d64e
Munna-Scripts <munnascriptz@gmail.com>
2025-10-08 22:26:30 +0600

    Search filter added

--- app/components/common/Navbar.jsx
+++ app/components/common/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import React, { useState , startTransition, useEffect  } from 'react'
 import logo from '../../../public/logo.svg'
 import Image from 'next/image'
-import { IoSearch } from "react-icons/io5";
+import { IoArrowRedoOutline, IoSearch } from "react-icons/io5";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { RxCross2 } from "react-icons/rx";
@@ -12,6 +12,7 @@ import { useProgress } from "react-transition-progress";
 import { useDispatch, useSelector } from 'react-redux'
 import { CartReducer } from '@/app/redux/cartSlice'
 import { FiImage } from 'react-icons/fi'
+import SearchFilter from './SearchFilter'
 const Navbar = () => {
     const startProgress = useProgress();
     const handleDelay = async ()=>{
@@ -35,10 +36,10 @@ const Navbar = () => {
   
 
     //   ------------------------ Handle search 
-    const [search , setSearch] = useState(false)
-    const handleSearch = ()=>{
-        console.log('hellow orld')
-    }
+    const [search , setSearch] = useState(true)
+    const [clear , setClear] = useState([])
+    
+    
   return (
     <section>
         <nav className='py-3 bg-brand lg:block hidden'>
@@ -70,20 +71,8 @@ const Navbar = () => {
         </nav>
 
         {/* -----Search----- */}
-        <div className={`${search? 'top-20' : '-top-30 '} absolute duration-500 right-10 bg-brand px-[32px] pt-[20px] pb-[30px] z-10 rounded-[12px]`}>
-            <fieldset className='md:w-[370px] w-full h-[64px] pb-3 border-1 border-[#8D918C] rounded-[8px]'>
-                <legend className='text-subText text-base ml-6 px-2'>Search</legend>
-                <div className='flex items-center justify-between h-full w-full px-3'>
-                    <div className='flex items-center h-full w-full'>
-                        <IoSearch className='text-2xl text-subText' />
-                        <input className='h-full pl-3 text-second w-full outline-none' placeholder='Search All Assets' type="search" id="search" />
-                    </div>
-                    <div className='text-2xl cursor-pointer text-subText'>
-                        <FiImage />
-                    </div>
-                </div>
-            </fieldset>
-        </div>
+        <SearchFilter searchHook={search} searchSetHook={setSearch} />
+
         {/* -----Account----- */}
       <section className={`w-full h-full backdrop-blur-md bg-[#00000063] z-50 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] duration-[.3s] flex items-center justify-center gap-7 ${value? 'hidden opacity-0 duration-75' : 'block opacity-100'}`}>
         <LoginOrRegis HideByLink={()=>(setValue(!value))}/>
---
+++ app/components/common/SearchFilter.jsx
@@ -0,0 +1,76 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import { IoArrowRedoOutline, IoSearch } from 'react-icons/io5'
+import { CgArrowTopLeft } from "react-icons/cg";
+
+
+const SearchFilter = ({searchHook , searchSetHook}) => {
+    // ---------------------- Catch the input data 
+    const [value , setValue] = useState('')
+
+    // ---------------------- Api -------------------------
+    const [product , setProduct] = useState([])
+    
+    useEffect(()=>{
+
+      const handleApi = async ()=>{
+
+        const response = await fetch('https://dummyjson.com/products/categories')
+
+        try{
+          const result = await response.json()
+          setProduct(result)
+        }catch(err){
+          console.log(err)
+        }
+        
+      }
+
+      handleApi()
+    }, [])
+    
+
+    // --------------------------- FIltering category by search 
+    const [searchPro , setSearchPro] = useState([]) 
+
+    const handleSearch = ()=>{
+        const categoryList = product.filter((items)=> items.name.toLowerCase().includes(value.toLocaleLowerCase()))
+        setSearchPro(categoryList)
+    }
+
+  return (
+    <>
+        <section className={`${searchHook? 'top-20' : '-top-40 '} absolute duration-500 right-10 bg-brand pt-[20px] pb-[10px] z-10 rounded-[12px]`}>
+            <form className='px-[32px]'>
+                <fieldset className='md:w-[370px] w-full h-[64px] pb-3 border-1 border-[#8D918C] rounded-[8px]'>
+                    <legend className='text-subText text-base ml-6 px-2'>Search</legend>
+                    <div className='flex items-center justify-between h-full w-full px-3'>
+                        <div className='flex items-center h-full w-full'>
+                            <IoSearch className='text-2xl text-subText' />
+                            <input onChange={(e)=>{setValue(e.target.value) , handleSearch()}} value={value} className='h-full pl-3 text-second w-full outline-none' placeholder='Search All Assets' type="search" id="search" />
+                        </div>
+                        <button type='submit' className='text-2xl cursor-pointer text-subText'>
+                            <IoArrowRedoOutline />
+                        </button>
+                    </div>
+                </fieldset>
+            </form>
+
+            {/* -------------- APi categories  */}
+            <div className={`${value == ''? 'h-auto' : 'h-[480px]'} mt-6  overflow-scroll overflow-x-hidden`}>
+                {
+                    searchPro.map((item , i)=>(
+                        <div key={i} className='px-[24px] py-[12px] border-b-1 cursor-pointer border-gray-300 flex items-center justify-between bg-[#e1ffb3] hover:bg-[#caff7a] duration-300 '>
+                            <h2 className='text-second text-lg font-medium'>{item.name}</h2>
+                            <CgArrowTopLeft className='text-2xl'/>
+                        </div>
+                    ))
+                }
+
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default SearchFilter
