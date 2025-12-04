c149d23302692eeba9d6050c82489dbc1f3bfbee
Munna-Scripts <munnascriptz@gmail.com>
2025-12-04 13:32:56 +0600

    Unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
+  images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "**" },
       { protocol: "http", hostname: "**" },
     ],
-
   },
 };
 
